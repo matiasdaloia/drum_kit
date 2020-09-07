75d8ef3c6c030b5036bc541a6f10406cf43588dd
matiasdaloia <66310421+matiasdaloia@users.noreply.github.com>
2020-09-07 18:55:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 //Audio objects
-var tom1 = new Audio("/sounds/tom-1.mp3");
-var tom2 = new Audio("/sounds/tom-2.mp3");
-var tom3 = new Audio("/sounds/tom-3.mp3");
-var tom4 = new Audio("/sounds/tom-4.mp3");
-var kickBass = new Audio("/sounds/kick-bass.mp3");
-var snare = new Audio("/sounds/snare.mp3");
-var crash = new Audio("/sounds/crash.mp3");
+var tom1 = new Audio("sounds/tom-1.mp3");
+var tom2 = new Audio("sounds/tom-2.mp3");
+var tom3 = new Audio("sounds/tom-3.mp3");
+var tom4 = new Audio("sounds/tom-4.mp3");
+var kickBass = new Audio("sounds/kick-bass.mp3");
+var snare = new Audio("sounds/snare.mp3");
+var crash = new Audio("sounds/crash.mp3");
 
 //Defining a function to play corresponding sound : 
 
